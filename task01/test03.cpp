fa48eee7208dbf9dff7a2b75a3d611cbe3c0c726
polin-sv <polinasvetlay@icloud.com>
2026-05-04 17:44:39 +0300

    test03

---
+++ task01/test03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+
+	cout << "input size of matrix(n abd m): ";
+	cin >> n >> m;
+
+	int** matrix = new int* [n];
+
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = new int[m];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			cout << matrix[i][ j]<< " ";
+		}
+
+		cout << "\n";
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[](*(matrix + i));
+	}
+
+	delete[] matrix;
+
+	return 0;
+}
